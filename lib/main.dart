51847d01d8ee9b54d9a3cdeaf27df4557f5797b8
Yasin ilhan <yasinilhan61@gmail.com>
2019-01-04 00:06:48 +0300

    initial process

---
+++ lib/main.dart
@@ -0,0 +1,156 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+  TabController _tabController;
+  ScrollController _scrollViewController;
+  AnimationController _animationController;
+
+  bool showNavBar = true;
+
+  _scrollListener() {
+    if (_scrollViewController.position.userScrollDirection.toString() ==
+        "ScrollDirection.reverse") {
+      setState(() {
+        showNavBar = false;
+      });
+    } else {
+      setState(() {
+        showNavBar = true;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(vsync: this, length: 2);
+    _scrollViewController = ScrollController();
+    _scrollViewController.addListener(_scrollListener);
+    _animationController = new AnimationController(
+      duration: const Duration(milliseconds: 1000),
+      vsync: this,
+    );
+    _animationController.addListener(() {
+      setState(() {});
+    });
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    _scrollViewController.dispose();
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: NestedScrollView(
+        controller: _scrollViewController,
+        headerSliverBuilder: (BuildContext context, bool boxIsScrolled) {
+          return <Widget>[
+            SliverAppBar(
+              title: Text('Tab Controller Example'),
+              pinned: true,
+              floating: true,
+              forceElevated: boxIsScrolled,
+              bottom: TabBar(
+                tabs: <Widget>[
+                  Tab(
+                    text: "Home",
+                  ),
+                  Tab(
+                    text: "Second page",
+                  ),
+                ],
+                controller: _tabController,
+              ),
+            )
+          ];
+        },
+        body: TabBarView(
+          children: <Widget>[
+            PageOne(),
+            PageTwo(),
+          ],
+          controller: _tabController,
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: 0, // this will be set when a new tab is tapped
+        items: [
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.home),
+            title: new Text('Home'),
+          ),
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.mail),
+            title: new Text('Messages'),
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person), title: Text('Profile'))
+        ],
+      ),
+    );
+  }
+}
+
+class PageOne extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Image.network(
+            'https://cdn.pixabay.com/photo/2018/01/12/10/19/fantasy-3077928_1280.jpg',
+            fit: BoxFit.cover,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class PageTwo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemExtent: 250.0,
+      itemBuilder: (context, index) => Container(
+            padding: EdgeInsets.all(10.0),
+            child: Material(
+              elevation: 4.0,
+              borderRadius: BorderRadius.circular(5.0),
+              color: index % 2 == 0 ? Colors.cyan : Colors.deepOrange,
+              child: Center(
+                child: Text(index.toString()),
+              ),
+            ),
+          ),
+    );
+  }
+}
